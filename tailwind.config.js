38c37d84867339b3caf6382ac57715a40c8969a1
Josue Hernandez <josuejeunihp@gmail.com>
2025-06-15 17:31:12 -0400

    Cambios en el diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,26 +3,25 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         "Color-principal": "hsl(222, 47%, 11%)",
-        "color-secundario": "hsl(189, 99%, 70%)"
+        "color-secundario": "hsl(189, 99%, 70%)",
       },
 
-      fontFamily:{
-        "principal": ['"Be Vietnam Pro"', 'sans-serif']
+      fontFamily: {
+        principal: ['"Be Vietnam Pro"', "sans-serif"],
       },
 
-      backgroundImage:{
+      backgroundImage: {
         "close-menu": "url('../images/icon-close.svg')",
         "open-menu": "url('../images/icon-hamburger.svg')",
-        "imagen-perfil": "url('../images/Foto_Josue.JPG')"
+        "imagen-perfil": "url('../images/linkeperfil.png')",
       },
 
-      maxWidth:{
-        "50ch": "50ch"
+      maxWidth: {
+        "50ch": "50ch",
       },
     },
   },
   plugins: [],
-}
-
+};
